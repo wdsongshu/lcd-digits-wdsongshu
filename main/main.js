a152db80fac6126f41f261b2a6411d3b257db5ce
wangdan <1253864861@qq.com>
2016-05-08 14:31:30 +0800

    changed in 5.8

---
+++ main/main.js
@@ -0,0 +1,69 @@
+function printReceipt(inputs) {
+
+    var splitedString = buildSplitedString(inputs);
+
+    var lcdNumberItems = loadAllItems();
+    var printItems = buildPrintItems(splitedString, lcdNumberItems);
+
+    var printText = buildPrint(printItems);
+
+}
+
+
+function buildSplitedString(inputs) {
+
+    var splitedString = inputs.split('');
+
+    return splitedString;
+}
+
+
+function buildPrintItems(splitedString, lcdNumberItems) {
+    var printItems = [];
+
+    splitedString.forEach(function (splitedNumber) {
+        var item = findLcdNumber(splitedNumber, lcdNumberItems);
+        if (item) {
+            printItems.push(item);
+        }
+    });
+
+    return printItems;
+}
+
+
+function findLcdNumber(splitedNumber, lcdNumberItems) {
+    var flag = false;
+
+    lcdNumberItems.forEach(function (lcdNumberItem) {
+        if (splitedNumber == lcdNumberItem.number) {
+            flag = lcdNumberItem;
+        }
+    });
+
+    return flag;
+}
+
+
+function buildPrint(printItems) {
+    var printText ='';
+
+    for(var i = 0 ; i < printItems.length;i++) {
+        var print ='';
+        printItems.forEach(function (printItem) {
+            var item = printItem.lcdNumber[i];
+            if(!print)
+            {
+                print += item ;
+            }else
+            {
+                print += ' ' + item ;
+            }
+
+        });
+        console.log(print);
+        printText += print + '\n';
+    }
+
+    console.log(printText) ;
+}
